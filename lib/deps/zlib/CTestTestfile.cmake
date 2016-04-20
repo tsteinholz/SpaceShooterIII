acaa2b70255162d26c73fc6ea1727fa4b35cb8a0
Thomas Steinholz <tsteinholz@yahoo.com>
2016-04-20 07:53:37 -0400

    removed deps/

--- lib/deps/zlib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/C++/SpaceShooterIII/lib/zlib
-# Build directory: H:/C++/SpaceShooterIII/lib/deps/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
